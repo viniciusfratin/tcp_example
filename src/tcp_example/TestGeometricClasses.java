463f4c1fce4be57c8c270af58836f7fafb0e88b4
Vinícius Fratin <viniciusfratin@hotmail.com>
2017-04-25 11:04:33 -0300

    Added another message.'

--- src/tcp_example/TestGeometricClasses.java
+++ src/tcp_example/TestGeometricClasses.java
@@ -7,14 +7,15 @@ public class TestGeometricClasses {
 		SquareShape square = new SquareShape(30);
 		TriangleShape triangle = new TriangleShape(3, 4, 5);
 		CircleShape circle = new CircleShape(1);
-		
+
 		System.out.println("Testing classes:");
 		System.out.println(rectangle.toString());
 		System.out.println(square.toString());
 		System.out.println(triangle.toString());
 		System.out.println(circle.toString());
-		
+
 		System.out.println("Bye!");
+		System.out.println("!!!");
 	}
 
 }
